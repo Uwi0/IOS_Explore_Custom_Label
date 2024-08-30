b918b96d2c5e75205a51f6099669a1f5fd552a19
uwi0 <kakapoweb@gmail.com>
2024-08-30 10:57:06 +0700

    Initial Commit

---
+++ Explore Custom Label/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Explore Custom Label
+//
+//  Created by dwi prasetyo on 30/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Explore Custom Label/Explore_Custom_LabelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Explore_Custom_LabelApp.swift
+//  Explore Custom Label
+//
+//  Created by dwi prasetyo on 30/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Explore_Custom_LabelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
